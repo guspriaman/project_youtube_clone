03891ce2e9a11452008a9385d4f0c054d5bf0dfe
guspriaman <email.guswarasi@gmail.com>
2024-07-11 15:37:05 +0700

    first commit

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -13,20 +13,59 @@ const Feed = () => {
 
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
       .then((data) => setVideos(data.items))
-    }, [selectedCategory]);
+  }, [selectedCategory]);
 
   return (
-    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+    <Stack 
+      sx={{ 
+        flexDirection: { sx: "column", md: "row" }, 
+        bgcolor: "#121212", 
+        minHeight: "100vh" 
+      }}
+    >
+      <Box 
+        sx={{ 
+          height: { sx: "auto", md: "92vh" }, 
+          borderRight: "1px solid #3d3d3d", 
+          px: { sx: 0, md: 2 }, 
+          bgcolor: "#1e1e1e"
+        }}
+      >
+        <Sidebar 
+          selectedCategory={selectedCategory} 
+          setSelectedCategory={setSelectedCategory} 
+        />
         
-        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+        <Typography 
+          className="copyright" 
+          variant="body2" 
+          sx={{ 
+            mt: 1.5, 
+            color: "#fff", 
+            textAlign: "center" 
+          }}
+        >
           Copyright © 2022 JSM Media
         </Typography>
       </Box>
 
-      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+      <Box 
+        p={2} 
+        sx={{ 
+          overflowY: "auto", 
+          height: "90vh", 
+          flex: 2, 
+          bgcolor: "#181818" 
+        }}
+      >
+        <Typography 
+          variant="h4" 
+          fontWeight="bold" 
+          mb={2} 
+          sx={{ 
+            color: "white" 
+          }}
+        >
           {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
 
